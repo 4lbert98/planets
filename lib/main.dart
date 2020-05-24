c1fa1ac4172aab37188f614fd4b2f4ae4dd98903
4lbert98 <65789582+4lbert98@users.noreply.github.com>
2020-05-24 16:02:09 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:planets/pages/home_page.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: "Planets",
+    home: HomePage(),
+  )
+);
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:planets/components/gradient_app_bar.dart';
+import 'package:planets/components/planets_list.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          GradientAppBar(),
+          PlanetList()
+        ],
+      ),
+    );
+  }
+}
